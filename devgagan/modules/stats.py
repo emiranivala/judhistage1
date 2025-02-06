ff475b2b21672ec44f1216b9c13de36225cab043
Judhistir <70930235+judhistir88@users.noreply.github.com>
2025-02-06 10:15:35 +0530

    Update stats.py

--- devgagan/modules/stats.py
+++ devgagan/modules/stats.py
@@ -22,8 +22,8 @@ from pyrogram import filters
 from config import OWNER_ID
 from devgagan.core.mongo.users_db import get_users, add_user, get_user
 from devgagan.core.mongo.plans_db import premium_users
-from pyrogram.types import InputFile
-import os
+
+
 
 start_time = time.time()
 
@@ -36,6 +36,8 @@ async def chat_watcher_func(_, message):
                 await add_user(message.from_user.id)
     except:
         pass
+
+
 
 def time_formatter():
     minutes, seconds = divmod(int(time.time() - start_time), 60)
@@ -57,60 +59,23 @@ def time_formatter():
     else:
         return "0 s"
 
-@app.on_message(filters.command("stats"))
+
+@app.on_message(filters.command("stats") & filters.user(OWNER_ID))
 async def stats(client, message):
     start = time.time()
-    users = await get_users()
+    users = len(await get_users())
     premium = await premium_users()
     ping = round((time.time() - start) * 1000)
-    
-    if message.from_user.id in OWNER_ID:
-        # Create a file with user IDs and usernames
-        file_path = "user_stats.txt"
-        with open(file_path, "w") as file:
-            file.write("User IDs and Usernames:\n")
-            for user in users:
-                user_info = await client.get_users(user['_id'])
-                file.write(f"{user['_id']} - {user_info.first_name}\n")
-
-        # Send the file to the owner
-        sent_message = await client.send_document(
-            chat_id=OWNER_ID[0],
-            document=InputFile(file_path),
-            caption="Here are the user IDs and usernames."
-        )
-        
-        # Get the link of the sent file
-        file_link = sent_message.link
-
-        # Send the stats message with the file link
-        await message.reply_text(f"""
+    await message.reply_text(f"""
 **Stats of** {(await client.get_me()).mention} :
 
 🏓 **Ping Pong**: {ping}ms
 
-📊 **Total Users** : `{len(users)}`
-📈 **Premium Users** : `{len(premium)}`
-⚙️ **Bot Uptime** : `{time_formatter()}`
-    
-📄 **User Details File**: [Download here]({file_link})
-    
-🎨 **Python Version**: `{sys.version.split()[0]}`
-📑 **Mongo Version**: `{motor.version}`
-""")
-        
-        # Remove the file after sending
-        os.remove(file_path)
-    else:
-        await message.reply_text(f"""
-**Stats of** {(await client.get_me()).mention} :
-
-🏓 **Ping Pong**: {ping}ms
-
-📊 **Total Users** : `{len(users)}`
+📊 **Total Users** : `{users}`
 📈 **Premium Users** : `{len(premium)}`
 ⚙️ **Bot Uptime** : `{time_formatter()}`
     
 🎨 **Python Version**: `{sys.version.split()[0]}`
 📑 **Mongo Version**: `{motor.version}`
 """)
+  
