3611cc431c4f8f9ee68c0a5decc526722a490ff9
emiranivala <taitufam@gmail.com>
2025-02-09 11:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "28777385"))
-API_HASH = getenv("API_HASH", "acf3ad8683c4475aa0e33910904eab82")
+API_ID = int(getenv("API_ID", "24210243"))
+API_HASH = getenv("API_HASH", "509031fb3790b968e489f71d591ebce5")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002362885377")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002437305944"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002458919549")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002294535138"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000000000000"))
 WEBSITE_URL = getenv("WEBSITE_URL", None)
